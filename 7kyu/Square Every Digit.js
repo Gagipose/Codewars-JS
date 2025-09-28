e2acd4782e997d46487e0921bfc3172003a67023
David Lindblom <lindblom.david@hotmail.com>
2025-09-28 16:34:04 +0200

    added first step of my soulution

--- 7kyu/Square Every Digit.js
+++ 7kyu/Square Every Digit.js
@@ -1,1 +1,15 @@
 // https://www.codewars.com/kata/546e2562b03326a88e000020
+
+// get integer number
+// turn into string then array
+// map each so it returns them squared
+// add togeather array items as a string
+// turn string into integer Number()
+
+
+// get integer number
+function squareDigits(num){
+    // turn into string then array
+    let numArray = Array.from(num.toString())
+  return 0;
+}
